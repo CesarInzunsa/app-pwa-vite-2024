3f073c7d33ffbc7afdacc0eeac6e7b5d3f739d0b
Cesar Inzunsa <cealinzunsadi@ittepic.edu.mx>
2024-04-15 14:37:52 -0700

    nota 6.4 terminada

---
+++ src/AppAllModules.jsx
@@ -0,0 +1,20 @@
+import {RouterProvider} from "react-router-dom";
+// Routes
+import router from "./navigation/NaviRoutesSecurity";
+//import  router from "./navigation/NaviRoutesEducation";
+//import  router from "./navigation/NaviRoutesCommerce";
+import Footer from "./share/footer/components/Footer";
+
+export default function AppAllModules() {
+    return (
+        <>
+            <div id='div-app'>
+                {/*<h1>Main App - All Modules</h1>*/}
+                <RouterProvider router={router}/>
+                <div id='div-footer'>
+                    <Footer/>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ src/ecommerce/orders/pages/Orders.jsx
@@ -0,0 +1,7 @@
+export default function Orders() {
+    return (
+        <div id="div-module">
+            <h1>Orders - Clients, Details products</h1>
+        </div>
+    );
+}
